fc17d726b27ffeb8bad6a96d740102ead4dc0d24
ArthurSiqueiraS <asesilva@inf.ufpel.edu.br>
2020-09-16 14:17:04 -0300

    Fixed FRONT_END const

--- config/application.rb
+++ config/application.rb
@@ -18,11 +18,13 @@ load 'app/lib/helpers.rb'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 class Application < Rails::Application
   # Initialize configuration defaults for originally generated Rails version.
   config.load_defaults 5.2
   config.action_cable.mount_path = '/cable'
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  FRONT_END = ENV.fetch('FRONT_END') { 'http://localhost:8080' }
 
   config.middleware.insert_before 0, Rack::Cors do
     allow do
