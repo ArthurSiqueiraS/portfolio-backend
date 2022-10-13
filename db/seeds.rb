df280ee514014814d7be4580640998978465171f
ArthurSiqueiraS <asesilva@inf.ufpel.edu.br>
2022-10-13 10:59:14 -0300

    fix react js

--- db/seeds.rb
+++ db/seeds.rb
@@ -272,17 +272,17 @@ technologies = {
   }),
   reactjs: create(Technology,
   {
-    name: 'React Native',
-    type: 'front_end',
-    logo: 'https://cdn.worldvectorlogo.com/logos/react-native-1.svg',
-    link: 'https://reactnative.dev/'
-  }),
-  reactnative: create(Technology,
-  {
     name: 'React.js',
     type: 'front_end',
     logo: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K',
     link: 'https://reactjs.org/'
+  }),
+  reactnative: create(Technology,
+  {
+    name: 'React Native',
+    type: 'front_end',
+    logo: 'https://cdn.worldvectorlogo.com/logos/react-native-1.svg',
+    link: 'https://reactnative.dev/'
   }),
   vuex: create(Technology,
   {
